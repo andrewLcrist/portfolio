cbd66f2b5a5324d684ac17543e1ca901d405fd27
Andrew Crist <andrew.l.crist@gmail.com>
2017-05-31 16:31:58 -0500

    Make multiple http requests

--- app/routes/home/projects.js
+++ app/routes/home/projects.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
   model(){
-    return $.get(`https://api.github.com/users/andrewlcrist`)
+    return RSVP.hash({
+      user: $.get('https://api.github.com/users/andrewlcrist'),
+      repos: $.get('https://api.github.com/users/andrewlcrist/repos')
+    })
   }
 });
