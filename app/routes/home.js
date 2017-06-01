c5d43ee09059497a0fb0e201d301882420eeed70
Andrew Crist <andrew.l.crist@gmail.com>
2017-06-01 11:57:19 -0500

    add about me

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
+import env from '../config/environment'
 
 export default Ember.Route.extend({
+  model(){
+    return $.get(`https://api.github.com/users/andrewlcrist?access_token=${env.githubToken}`)
+  }
 });
