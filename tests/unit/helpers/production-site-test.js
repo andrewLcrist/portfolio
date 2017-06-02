f500ff5436943fd5d66956e16b559a44a6c70cbc
Andrew Crist <andrew.l.crist@gmail.com>
2017-06-01 19:49:35 -0500

    Add production helper

---
+++ tests/unit/helpers/production-site-test.js
@@ -0,0 +1,12 @@
+
+import { productionSite } from 'portfolio/helpers/production-site';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | production site');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = productionSite([42]);
+  assert.ok(result);
+});
+
